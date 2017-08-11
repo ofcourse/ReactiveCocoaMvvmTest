408b4b4fe0a1ce1d7968dd7da65fed2a26d7e54b
ios6 <ios6@h5deMac-mini.local>
2017-08-11 10:21:37 +0800

    Initial Commit

---
+++ ReactiveCocoaMvvmTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReactiveCocoaMvvmTest
+//
+//  Created by ios6 on 2017/8/11.
+//  Copyright © 2017年 ios6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReactiveCocoaMvvmTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactiveCocoaMvvmTest
+//
+//  Created by ios6 on 2017/8/11.
+//  Copyright © 2017年 ios6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
